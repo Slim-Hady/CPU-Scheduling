14505ec0a9200e31ca58079eb64925f634a02ac9
Slim-Hady <mohamed.elsogher.2024@aiu.edu.eg>
2026-02-15 00:55:05 +0200

    Final commit

--- main.c
+++ main.c
@@ -6,15 +6,39 @@
 int main() {
     process p[100];
     int n;
+    int choice;
 
     readFile("input.txt", p, &n); 
-    printf("Successfully read %d processes.\n", n);
+    
+    printf("\n--- CPU Scheduler ---\n");
+    printf("1. FCFS\n");
+    printf("2. SJF\n");
+    printf("3. RR\n");
+    printf("Choose algorithm: ");
+    scanf("%d", &choice);
 
-    int quantum = 2; 
-    rr(p, n, quantum);
+    switch(choice) {
+        case 1:
+            FCFS(p, n);
+            break;
+        case 2:
+            sjf(p, n);
+            break;
+        case 3:
+            {
+                int quantum;
+                printf("Enter Quantum: ");
+                scanf("%d", &quantum);
+                rr(p, n, quantum);
+            }
+            break;
+        default:
+            printf("Invalid Choice!\n");
+            return 1;
+    }
 
     writeFile("output.txt", p, n);
-    printf("Output file created.\n");
+    printf("\nDone! Results saved to output.txt\n");
     
     return 0;
 }
