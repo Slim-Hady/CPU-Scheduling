b72154c6aaccf44c3b522c522f6fc2e801cf3ec5
Slim-Hady <mohamed.elsogher.2024@aiu.edu.eg>
2026-02-15 00:41:53 +0200

    add round robin

---
+++ src/rr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "../include/schedulers.h"
+#include "../include/utils.h"
+
+void rr(process p[] , int n , int quantum){
+    sort(p , n);
+    for(int i = 0; i < n;i++){
+        p[i].remaining_time = p[i].burst_time;
+    }
+    int curr = 0;
+    int completed = 0;
+    while(completed != n){
+        bool work = false;
+        for(int i =0;i<n;i++){
+            if(p[i].remaining_time > 0 && p[i].arrival_time <= curr){
+                work = true;
+                if(p[i].remaining_time > quantum){
+                    p[i].remaining_time -= quantum;
+                    curr += quantum;
+                }
+                else {
+                    curr += p[i].remaining_time;
+                    p[i].remaining_time = 0;
+                    completed++;
+                    p[i].completion_time = curr;
+                }
+            }
+        }
+        if (work == false) {
+        curr++; 
+    }
+    }
+}
